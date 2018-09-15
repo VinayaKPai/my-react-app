9b9566309bd468af1b0c3bbdd0e2b753a5327f0f
Vinaya <vinayakeshavpai@gmail.com>
2018-09-16 00:04:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './src/index.css';
+import App from './src/App';
+import registerServiceWorker from './src/registerServiceWorker';
+
+//import './src/index.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
